bb171c1e5234547cce901df2eaa47a53532e8ca5
eshaalal <eshalal888@gmail.com>
2024-08-14 11:38:43 +0530

    changes

--- code2.php
+++ code2.php
@@ -15,7 +15,7 @@ echo($x**$y);
 echo "<br>";
 ?> -->
 
-<?php
+<!-- <?php
 $x=25;
 $y=35;
 $z="24";
@@ -28,4 +28,99 @@ var_dump($x<$y);
 var_dump($x>$z);
 
 
+?> -->
+
+<!-- <html>
+<body>
+<h2>Type Casting </h2>
+<pre>
+<?php
+$a=5;
+$b=5.454;
+$c="hello";
+$d=true;
+$e=NULL;
+
+$a=(string) $a;
+$b=(string) $b;
+$c=(string) $c;
+$d=(string) $d;
+$e=(string) $e;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+
+
 ?>
+</pre>
+<p>dnkdjc</p>
+</body>
+</html> -->
+
+
+<!-- <!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+
+$company = "javatpoint";
+
+echo"<h1>Double qoute representation<br></h1>";
+
+echo"Hello $company";
+
+echo"</br>";
+echo"</br>";
+
+echo "Single quote reprentation</br>";
+
+echo 'Hello $company';
+
+
+
+?> 
+
+</body>
+</html> -->
+
+
+<!-- <!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+
+$num=12;
+if($num%2==0) {
+echo"$num is a even number";
+} else {
+echo"$num is a odd number";
+}
+?> 
+
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+
+$t=date("H");
+if($t<"20") {
+echo"Have a good day";
+} else {
+echo"Have a good night";
+}
+?> 
+
+</body>
+</html>
