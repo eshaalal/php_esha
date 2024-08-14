35885cc6919889d7fe9f48ce91a7bb903b3fde9a
eshaalal <eshalal888@gmail.com>
2024-08-14 11:04:45 +0530

    changes

---
+++ code2.php
@@ -0,0 +1,31 @@
+<!-- <?php
+$x=10;
+$y=4;
+echo($x+$y);
+echo "<br>";
+echo($x-$y);
+echo "<br>";
+echo($x*$y);
+echo "<br>";
+echo($x/$y);
+echo "<br>";
+echo($x%$y);
+echo "<br>";
+echo($x**$y);
+echo "<br>";
+?> -->
+
+<?php
+$x=25;
+$y=35;
+$z="24";
+var_dump($x==$z);
+var_dump($x===$z);
+var_dump($x!=$y);
+var_dump($x>=$z);
+var_dump($x<=$z);
+var_dump($x<$y);
+var_dump($x>$z);
+
+
+?>
