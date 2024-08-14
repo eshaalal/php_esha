ea04f14a0a30b748564391f1b7a37601b3f3ef27
eshaalal <eshalal888@gmail.com>
2024-08-14 10:08:36 +0530

    changes

---
+++ code1.php
@@ -0,0 +1,24 @@
+<?php
+echo "<h2>HELLO WE ARE GOING TO START WITH PHP</h2>";
+echo "<br>";
+echo "HELLO STUDENTS<br>";
+echo "<h2>PHP is fun!</h2>";
+echo "HELLO WORLD<br>";
+echo "I am about to learn php<br>";
+echo "This ","string ", "was ","made ","with multiple parameter";
+echo "<br>";
+
+echo 'THIS IS SINGLE COATED STRING';
+echo "<br>";
+echo "<br>";
+
+// $x=10;
+// $y=6;
+// echo "<h2>DIFFERENT WAY TO PRINT</h2>";
+// echo $x + $y;
+// echo "<br>";
+// echo "$x+$y";
+// echo "<br>";
+// echo "$x"+"$y";
+?> 
+
