d41f5f3f87abe0c053251583863d01e73781efc8
eshaalal <eshalal888@gmail.com>
2024-08-21 10:28:27 +0530

    changes

---
+++ code3.php
@@ -0,0 +1,23 @@
+<!-- <?php
+$num=10;
+if($num>5){
+    echo"$num is grater than 5";
+}
+?> -->
+
+<?php
+$fruit = "apple";
+
+if ($fruit == "apple") {
+    echo "This is an apple.";
+} elseif ($fruit == "banana") {
+    echo "This is a banana.";
+} elseif ($fruit == "orange") {
+    echo "This is an orange.";
+} elseif ($fruit == "grape") {
+    echo "This is a grape.";
+} else {
+    echo "This is some other fruit.";
+}
+?>
+
