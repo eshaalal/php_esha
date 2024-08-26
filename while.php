36932b943035e942daf371b1a720d555305bfe71
eshaalal <eshalal888@gmail.com>
2024-08-26 10:16:40 +0530

    changes

--- while.php
+++ while.php
@@ -1,8 +1,27 @@
-<?php
+<!-- <?php
 $number = 1;
 
 do {
     echo "Number: $number\n";
     $number++;
 } while ($number <= 5);
+?> -->
+
+
+<!-- <?php
+ $colors=array("Red","Green","Blue");
+ foreach($colors as $value){
+    echo $value,"<br>";
+ }
+?> -->
+
+<?php
+$n=6;
+$x=1;
+for($i=1;$i<=$n-1;$i++){
+    $x*=($i+1);
+}
+echo" The factorial of $n = $x"."\n";
+
+
 ?>
